fa7984a5bea3594493cdc873aa6763bff99dcf64
Luiz Felipe <luiizz.oliveira02@outlook.com>
2024-10-07 14:07:29 -0300

    galeria criada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Menu Mobile
 function menuShow() {
   let menuMobile = document.querySelector(".menu-mobile");
   if (menuMobile.classList.contains("open")) {
@@ -6,3 +7,44 @@ function menuShow() {
     menuMobile.classList.add("open");
   }
 }
+
+// Galeria
+const galeria = document.querySelector(".galeria");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+const imagensVisiveis = 4;
+const tamanhoImg = 300 + 32;
+
+let currentIndex = 0;
+const maxIndex =
+  document.querySelectorAll(".galeria-img").length - imagensVisiveis;
+
+function atualizaPosicaoGaleria() {
+  const posicao = -currentIndex * tamanhoImg;
+  galeria.style.transform = `translate(${posicao}px, -50%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  if (currentIndex < maxIndex) {
+    currentIndex++;
+    atualizaPosicaoGaleria();
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    atualizaPosicaoGaleria();
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight" && currentIndex < maxIndex) {
+    currentIndex++;
+    atualizaPosicaoGaleria();
+  } else if (e.key === "ArrowLeft" && currentIndex > 0) {
+    currentIndex--;
+    atualizaPosicaoGaleria();
+  }
+});
